71d50873e16e0bf201930421cd853b9341baca61
AzwadFawadHasan2020222 <azwadfawadhasan@gmail.com>
2023-10-07 23:37:28 +0600

    added search feature using select

--- searchDjangoProject/locations/views.py
+++ searchDjangoProject/locations/views.py
@@ -4,7 +4,16 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import Place
 
+from django.shortcuts import render
+from .models import Place
+
+from django.db.models import Q
+
 def search(request):
     query = request.GET.get('q', '')
-    places = Place.objects.filter(name__icontains=query)
+    places = []
+
+    if query:
+        places = Place.objects.filter(Q(name__icontains=query) | Q(state__name__icontains=query))
+
     return render(request, 'search.html', {'places': places, 'query': query})
